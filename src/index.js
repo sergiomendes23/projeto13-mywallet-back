0293e98bcc4d818aca83496cdb36bcb92c4defab
Sergio <sergiomendes23@gmail.com>
2022-09-10 23:50:59 -0300

    Rota cadastro e login ok

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { MongoClient } from 'mongodb'
 import dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
 
 const server = express();
 server.use(cors());
@@ -28,11 +29,40 @@ server.post('/cadastro', async (req,res) => {
 
     try {
         await db.collection('usuarios').insertOne({nome, email, senha: cryptSenha})
-        return res.send(200);
+        return res.sendStatus(200);
     }catch(error){
         console.log(error.message);
     }
     
 })
 
-server.listen(5000, () => {console.log('ouvindo a portinha 5000')}) 
+server.post('/login', async (req, res) => {
+    const {email, senha} = req.body;
+
+    const usuario = await db.collection('usuarios').findOne({email});
+    const senhaValida = bcrypt.compareSync(senha, usuario.senha);
+
+    if (!usuario && !senhaValida) {
+        return res.send(400);
+    }
+    try{
+
+        if (!senhaValida) {
+            return res.send(401);
+        }
+
+        const token = uuid()
+        await db.collection('sessions').insertOne({token, userId: usuario._id})
+
+        return res.send(token).status(200)
+       
+    }catch{
+        console.log(error.message);
+    }
+})
+
+
+
+
+
+server.listen(5001, () => {console.log('Ouvindo a porta 5001')}) 
